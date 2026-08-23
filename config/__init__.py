10ded7767c62a32ff252c3bc914bee5058baa428
黄海生 <huanghaisheng@maizicare.com>
2026-08-23 13:11:51 +0800

    fix: 修改 minimum_database_version 绕过版本检查

--- config/__init__.py
+++ config/__init__.py
@@ -2,12 +2,5 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 # 微信云托管 MySQL 5.7，但 Django 4.2+ 要求 8.0，绕过版本检查
-import django.db.backends.mysql.base
-_orig_get_new_connection = django.db.backends.mysql.base.DatabaseWrapper.get_new_connection
-
-def _patched_get_new_connection(self, conn_params):
-    conn = _orig_get_new_connection(self, conn_params)
-    conn.server_version = (8, 0, 0)  # 告知 Django 使用 8.0 特性
-    return conn
-
-django.db.backends.mysql.base.DatabaseWrapper.get_new_connection = _patched_get_new_connection
+from django.db.backends.mysql import features
+features.DatabaseFeatures.minimum_database_version = (5, 7, 0)
